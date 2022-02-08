f970abb09a6d504bfdafe964ee52fd54b2d97ba8
Amkhitha Liwela <kitaln22@gmail.com>
2022-02-08 19:47:15 +0200

    New Change

---
+++ TestProject/src/Pack/NewClass.java
@@ -0,0 +1,21 @@
+package Pack;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello");
+		
+		int i;
+		i = 6;
+		
+		if(i > 5)
+		{
+			System.out.print("It is greater than five");
+		}
+		
+		
+	}
+
+}
